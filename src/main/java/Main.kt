883ef1e7aa860ea5bec493a5959aa00e1154d5d8
Bobyshev Nikita <Gammanik@gmail.com>
2020-04-05 14:26:43 +0300

    implement naive suggestDriversAsync

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,32 +1,80 @@
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
+import io.ktor.client.features.json.GsonSerializer
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.request.get
+import kotlinx.coroutines.*
+import model.DistanceMatrix
+import model.Participants
+import model.Person
+import model.Point
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
 import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.pow
+import kotlin.math.sqrt
+import CONST.API_KEY
+import CONST.MAPS_BASE_URL
 
-
-data class Point(val latitude: Float, val longitude: Float)
-data class Participants(val passengers: Collection<Person>, val drivers: Collection<Person>)
-data class Person(val id: UUID, val finishPoint: Point)
-
-fun main() {
-    val (passengers, drivers) = readPoints()
-    for (passenger in passengers) {
-        val suggestedDrivers = suggestDrivers(passenger, drivers)
-        println("Passenger point: ${passenger.finishPoint.latitude}, ${passenger.finishPoint.longitude}")
-        for (driver in suggestedDrivers) {
-            println("  ${driver.finishPoint.latitude}, ${driver.finishPoint.longitude}")
+object CONST {
+    val API_KEY: String = Files.readString(Path.of("src/main/resources/API_KEY.txt"))
+    const val MAPS_BASE_URL: String = "https://maps.googleapis.com/maps/api/distancematrix/json"
+    val client = HttpClient(Apache) {
+        install(JsonFeature) {
+            serializer = GsonSerializer()
         }
     }
 }
 
-fun suggestDrivers(passenger: Person, drivers: Collection<Person>): Collection<Person> {
-    TODO("Implement me")
+fun main() {
+    val (passengers, drivers) = readPoints()
+    val responses: ArrayList<Deferred<Collection<Person>>> = ArrayList()
+
+    for (passenger in passengers) {
+        val suggestedDrivers = suggestDriversAsync(passenger, drivers)
+        println("Passenger point: ${passenger.finishPoint.latitude}, ${passenger.finishPoint.longitude}")
+        GlobalScope.launch { responses.add(suggestedDrivers) }
+    }
+
+    runBlocking {
+        val res: List<Collection<Person>> = responses.awaitAll()
+        for (el in res) {
+            println(el.map { "(${it.finishPoint})" })
+        }
+    }
+}
+
+fun suggestDriversAsync(passenger: Person, drivers: ArrayList<Person>): Deferred<Collection<Person>> =
+    GlobalScope.async { getDistancesAsync(passenger, drivers).await().rows[0].elements
+                .mapIndexed { i, el -> Pair(el.duration.value ,drivers[i]) }
+                .sortedBy { it.first }
+                .map { it.second }
+    }
+
+suspend fun getDistancesAsync(passenger: Person,
+                              drivers: Collection<Person>): Deferred<DistanceMatrix> = GlobalScope.async(Dispatchers.IO) {
+    val driversCoords: String = drivers.joinToString(separator = "%7C")
+        { "${it.finishPoint.latitude}%2C${it.finishPoint.longitude}" }
+
+    val urlStr = "${MAPS_BASE_URL}?origins=${passenger.finishPoint}&destinations=${driversCoords}&key=${API_KEY}"
+    CONST.client.get<DistanceMatrix>(urlStr)
+}
+
+// todo: cut drivers that are longer distance than radius
+fun getClosestDrivers(radius: Double) { }
+
+fun getDistance(p1: Point, p2: Point): Float {
+    return sqrt((p1.longitude - p1.latitude).pow(2) +
+            (p2.longitude - p2.latitude).pow(2))
 }
 
 private fun readPoints(): Participants {
-    val pathToResource = Paths.get(Point::class.java.getResource("latlons").toURI())
+    val pathToResource = Paths.get(Point::class.java.getResource("../latlons").toURI())
     val allPoints = Files.readAllLines(pathToResource).map { asPoint(it) }.shuffled()
     val passengers = allPoints.slice(0..9).map { Person(UUID.randomUUID(), it) }
-    val drivers = allPoints.slice(10..19).map { Person(UUID.randomUUID(), it) }
+    val drivers = ArrayList(allPoints.slice(10..19).map { Person(UUID.randomUUID(), it) })
     return Participants(passengers, drivers)
 }
 
---
+++ src/test/java/SuggestDriversTest.kt
@@ -0,0 +1,10 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class SuggestDriversTest {
+
+    @Test
+    fun testZeroResponse() {
+        assertEquals(2, 2)
+    }
+}
